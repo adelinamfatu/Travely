e8f6b157122a6cfc3c8baceee857d38d30e0d7ff
Adelina Fatu <adelinamfatu@gmail.com>
2024-03-04 22:45:51 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Travely
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
