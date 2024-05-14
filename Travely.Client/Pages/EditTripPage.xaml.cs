a344286af2f33738f1ced95f6791caf2aeb193c7
vircanraluca <vircan.raluca@yahoo.com>
2024-05-14 09:02:43 +0300

    Modified the cards  on the edit trip page to be expandable

--- Travely.Client/Pages/EditTripPage.xaml.cs
+++ Travely.Client/Pages/EditTripPage.xaml.cs
@@ -1,8 +1,104 @@
+using System.ComponentModel;
+
 namespace Travely.Client.Pages;
 
 public partial class EditTripPage : ContentPage
 {
-	public EditTripPage()
+    private bool isExpanded;
+
+    public bool IsExpanded
+    {
+        get { return isExpanded; }
+        set
+        {
+            if (isExpanded != value)
+            {
+                isExpanded = value;
+                OnPropertyChanged(nameof(IsExpanded));
+            }
+        }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    private bool isCollapsed = false;
+
+     private void HandleChevronPlacesDownClicked(object sender, EventArgs e)
+     {
+         if (isCollapsed)
+         {
+             placesFrame.HeightRequest = double.NaN;
+             isCollapsed = false;
+         }
+         else
+         {
+
+            placesFrame.HeightRequest = 100; 
+             isCollapsed = true;
+         }
+     }
+
+
+    private void HandleChevronNotesClicked(object sender, EventArgs e)
+    {
+        if (isCollapsed)
+        {
+            notesFrame.HeightRequest = double.NaN;
+            isCollapsed = false;
+        }
+        else
+        {
+
+            notesFrame.HeightRequest = 100;
+            isCollapsed = true;
+        }
+    }
+
+    private void HandleChevronRestaurantsClicked(object sender, EventArgs e)
+    {
+        if (isCollapsed)
+        {
+            restaurantsFrame.HeightRequest = double.NaN;
+            isCollapsed = false;
+        }
+        else
+        {
+
+            restaurantsFrame.HeightRequest = 100;
+            isCollapsed = true;
+        }
+    }
+
+
+
+
+    /*  private void HandleChevronDownClicked(object sender, EventArgs e)
+      {
+          foreach (var child in Content.FindByName<VerticalStackLayout>("mainLayout").Children)
+          {
+              if (child is Frame frame)
+              {
+                  if (isCollapsed)
+                  {
+                      frame.HeightRequest = double.NaN;
+                  }
+                  else
+                  {
+                      frame.HeightRequest = 100;
+                  }
+              }
+          }
+          isCollapsed = !isCollapsed;
+      }*/
+
+
+
+    public EditTripPage()
 	{
 		InitializeComponent();
 	}
