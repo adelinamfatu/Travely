840972c539bb86ad76b14c1edfe1d6ee73e11ee4
roxyenescu <115460507+roxyenescu@users.noreply.github.com>
2024-05-15 23:06:35 +0300

    Implemented checkbox and label layout in item list for Trip Packing Page

---
+++ Travely.Client/Models/PackingListViewModel.cs
@@ -0,0 +1,30 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Travely.Client.Models
+{
+    public partial class PackingListViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string? itemInput;
+
+        [ObservableProperty]
+        private ObservableCollection<string> items = new();
+
+        [RelayCommand]
+        private void AddItem()
+        {
+            if (!string.IsNullOrWhiteSpace(ItemInput))
+            {
+                Items.Add(ItemInput);
+                ItemInput = string.Empty;
+            }
+        }
+    }
+}
--- Travely.Client/Pages/TripPackingPage.xaml.cs
+++ Travely.Client/Pages/TripPackingPage.xaml.cs
@@ -1,9 +1,13 @@
 namespace Travely.Client.Pages;
 
+using System.Collections.ObjectModel;
+using Travely.Client.Models;
+
 public partial class TripPackingPage : ContentPage
 {
-	public TripPackingPage()
-	{
-		InitializeComponent();
-	}
+    public TripPackingPage()
+    {
+        InitializeComponent();
+        BindingContext = new PackingListViewModel();
+    }
 }
