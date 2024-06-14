ea802d2f55ea950703cafca70ff9d6eaffb65f9f
vircanraluca <vircan.raluca@yahoo.com>
2024-06-14 18:22:04 +0300

    Modified the API call and Dahsboard UI for API Countries Using EUR

--- Travely.BusinessLogic/Services/CountryService.cs
+++ Travely.BusinessLogic/Services/CountryService.cs
@@ -23,9 +23,20 @@ namespace Travely.BusinessLogic.Services
             var json = JObject.Parse(response);
             var countries = json["data"].ToObject<JArray>();
 
+            var euroCountries = new JArray();
+
+            foreach (var country in countries)
+            {
+                if (country["currency"].ToString().ToLower() == "eur")
+                {
+                    euroCountries.Add(country);
+                }
+            }
+
+            
             Console.WriteLine($"Number of countries fetched: {countries.Count}");
 
-            return countries;
+            return euroCountries;
         }
 
     }
